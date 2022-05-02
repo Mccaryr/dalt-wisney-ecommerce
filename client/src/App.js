cc0ca0b1844de677eee333b886460092ccc4db15
Mccary.Rob <Mccary.Rob@doc.sc.gov>
2022-05-02 10:19:57 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">      
+        {/* <Counter />     */}
+        <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
