d0a5eb0995f9cef75850b3ba08639c115e4bd3ce
Mccary.Rob <Mccary.Rob@doc.sc.gov>
2022-06-14 10:17:58 -0400

    Fixed clipping issues with mobile device UI

--- client/src/components/DisneyPlus/DisneyPlus.jsx
+++ client/src/components/DisneyPlus/DisneyPlus.jsx
@@ -7,21 +7,21 @@ const DisneyPlus = () => {
     <div className='container'>
       <div className="disney-bundle-container">
         <pre><h1>Hulu     Disney+     ESPN+</h1></pre>
-        <strong><p style={{fontSize:'2vw'}}>Enter your email to create or restart you subscription</p></strong>
+        <strong><p>Enter your email to create or restart you subscription</p></strong>
         <div className="email-container">
           <input type="text" placeholder="Email" /><button>GET THE DISNEY BUNDLE</button>
         </div>
       </div>
       <div className="headliner-ad">
-        <img src={HeadlinerAd} alt="headliner ad" />
+        <img src={HeadlinerAd} alt="headliner ad" />   
         <div className="text-container">
-        <h1>Watch the way you want</h1>
-        <ul>
-          <li>Host virtual movie nights with GroupWatch. Pause, rewind and react with up to six friends. Subscription is required to join GroupWatch.</li>
-          <li>Download any movie or series and watch on-the-go.</li>
-          <li>Enjoy an ever-growing range of titles in stunning 4K UHD and Dolby Atmos sound on compatible devices.</li>
-          <li>Stream on up to four devices at the same time.</li>
-        </ul>
+          <h1>Watch the way you want</h1>
+            <ul>
+              <li>Host virtual movie nights with GroupWatch. Pause, rewind and react with up to six friends. Subscription is required to join GroupWatch.</li>
+              <li>Download any movie or series and watch on-the-go.</li>
+              <li>Enjoy an ever-growing range of titles in stunning 4K UHD and Dolby Atmos sound on compatible devices.</li>
+              <li>Stream on up to four devices at the same time.</li>
+            </ul>
         </div>
       </div>
     </div>
